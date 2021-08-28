72cb797c63ece918b4a58ca9907609ddba469592
petar stoyanov <petur_stoqnov@abv.bg>
2021-08-28 14:04:33 +0300

    removed async pipe on home page

--- Game-app/src/app/components/landing/carousel-navigation/carousel-navigation.component.ts
+++ Game-app/src/app/components/landing/carousel-navigation/carousel-navigation.component.ts
@@ -1,20 +1,27 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Game} from '../../../core/models/game';
 import {GameService} from '../../../core/services/game.service';
-import {Observable} from 'rxjs';
+import {Observable, Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-carousel-navigation',
   templateUrl: './carousel-navigation.component.html',
   styleUrls: ['./carousel-navigation.component.css']
 })
-export class CarouselNavigationComponent implements OnInit {
-  game$: Observable<Array<Game>>;
+export class CarouselNavigationComponent implements OnInit, OnDestroy {
+  games: Game[] = [];
+  getAllGamesSubs: Subscription;
 
   constructor(private gameService: GameService) {
   }
 
-  ngOnInit(): void {
-    this.game$ = this.gameService.getAllGames();
+  ngOnInit() {
+    this.getAllGamesSubs = this.gameService.getAllGames().subscribe((data) => {
+      this.games = data.splice(0, 3);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.getAllGamesSubs.unsubscribe();
   }
 }
